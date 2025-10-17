6e72c8a2c9c7b18882f61a4cca98ed078b35ded4
matteovecchio04 <matteovex@gmail.com>
2025-10-17 17:18:51 +0200

    add for cycle that runs through the object array and the condition says to udate the control variable to the smallest weight number. add the result in DOM

--- snack-1/script.js
+++ snack-1/script.js
@@ -5,10 +5,29 @@ Stampare a schermo la bici con peso minore.
 */
 
 const bike = [
-    {nome: "Canyon", peso: 2}
-    {nome: "Piranello", peso: 3}
-    {nome: "Colnago", peso: 4}
-    {nome: "Specialized", peso: 5}
+    {nome: "Canyon", peso: 7},
+    {nome: "Piranello", peso: 3},
+    {nome: "Colnago", peso: 4},
+    {nome: "Specialized", peso: 5},
     {nome: "Trek", peso: 6}
 ]
 // object array
+
+for (let nome in bike) {
+    console.log(bike[nome]);
+}
+// controllare in log se l'object array funziona
+
+let lightBike = bike[0]
+// control variable
+for (i = 0; i < bike.length; i++) {
+    if (bike[i].peso < lightBike.peso) {
+        lightBike = bike[i]
+    }
+}
+
+console.log(`La bicicletta più leggera è la "${lightBike.nome}" con un peso di ${lightBike.peso}kg !`);
+
+const lightBikeEl = document.getElementById("lightBike")
+
+lightBikeEl.innerHTML = `La bicicletta più leggera è la "${lightBike.nome}" con un peso di ${lightBike.peso}kg !`
