289c7f69daf85d42b234fee3e6e51bf73a563b68
matteovecchio04 <matteovex@gmail.com>
2025-10-17 16:48:41 +0200

    add object array

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,3 +3,12 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore.
 */
+
+const bike = [
+    {nome: "Canyon", peso: 2}
+    {nome: "Piranello", peso: 3}
+    {nome: "Colnago", peso: 4}
+    {nome: "Specialized", peso: 5}
+    {nome: "Trek", peso: 6}
+]
+// object array
